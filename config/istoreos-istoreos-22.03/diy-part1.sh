3b9c4314c67268da4f8905e9ec281a8fb8a96535
Lemon <149338935+Lemon1151@users.noreply.github.com>
2024-10-12 09:15:24 +0800

    Update diy-part1.sh

--- config/istoreos-istoreos-22.03/diy-part1.sh
+++ config/istoreos-istoreos-22.03/diy-part1.sh
@@ -6,11 +6,6 @@
 # Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
 
-# Add a feed source
-# sed -i '$a\src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default  
-# sed -i '$a\src-git istore https://github.com/linkease/istore;main' feeds.conf.default  
-# sed -i '$a\src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default  
-# sed -i '$a\src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
